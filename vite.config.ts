1abf0441664ff2cb4c1566098eb6c60c29d5b3e8
xieerxuan <2724235894@qq.com>
2025-12-24 12:17:29 +0800

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/christmas-tree/', // 重要：添加这一行
       server: {
         port: 3000,
         host: '0.0.0.0',
